2f95e593f33a1f361429483dca09f015f5744bf1
Farhan Kathawala <farhank@stanford.edu>
2015-01-02 16:32:25 -0800

    Eliminated temp files. Shortened JSON formatting.
    Big shout out to reddit user "thestoicattack" for helping me understand sed a little better :)

--- gist.sh
+++ gist.sh
@@ -11,45 +11,17 @@ CONTENT=
 DESCRIPTION=
 PUBLIC="true"
 
-# These files get removed on exit, still looking for ways to omit them altogether
-JSON_FILE=/tmp/temporary_gist_json_file.json
-PARSE_FILE=/tmp/temporary_gist_parsing_file
-TMP=/tmp/temporary_gist_parsing_file.temp
-cleanup (){
-    rm ${JSON_FILE} &> /dev/null
-    rm ${PARSE_FILE} &> /dev/null
-    rm ${TMP} &> /dev/null
-}
-trap cleanup EXIT
-
 # This function takes a file as input argument and creates a valid
 # JSON string which is then set as the value of the variable CONTENT.
-# The sed and awk calls escape backslashes and double quotes and place
+# The sed call escapes backslashes and double quotes and places
 # literal newlines and literal tab characters (\n\t) where newlines and
 # tabs originally were in the text.
 function format_file_as_JSON_string() {
-    sed -e 's/\\/\\\\/g' < "${1}" > ${PARSE_FILE}
-    awk '{print $0"\\n"}' ${PARSE_FILE} > ${TMP} && mv ${TMP} ${PARSE_FILE}
-    awk '{gsub(/"/, "\\\"")} 1' < ${PARSE_FILE} > ${TMP} && mv ${TMP} ${PARSE_FILE}
-    sed 's/\t/\\t/g' < ${PARSE_FILE} > ${TMP} && mv ${TMP} ${PARSE_FILE}
-    cat /dev/null > ${TMP}
-
-    # This block encloses the file in double quotes and strips away all newlines
-    # so a file which looks like:
-    #    Hi\n
-    #      Indentation\n
-    #        More!\n
-    # would become a valid JSON string:
-    #    "Hi\n  Indentation\n    More!\n"
-    echo -n "\"" >> ${TMP}
-    IFS=''
-    while read -r data; do
-	line="$data"
-	echo -n "$line" >> ${TMP}
-    done < ${PARSE_FILE}
-    echo -n "\"" >> ${TMP} && mv ${TMP} ${PARSE_FILE}
-    
-    CONTENT=$(cat ${PARSE_FILE})
+    sed -e 's/\\/\\\\/g' \
+	-e 's/$/\\n/g' \
+	-e 's/"/\\"/g' \
+	-e 's/\t/\\t/g' \
+	| tr -d "\n"
 }
 
 
@@ -97,6 +69,7 @@ if [ $# -eq 0 ]; then
     Report bugs to: farhank@stanford.edu
     pkg home page:  <https://github.com/kathawala/gist/blob/master/gist.sh>
 EOF
+
     exit 1
 fi
 
@@ -111,7 +84,7 @@ else
     if [ -z "${FILENAME}" ]; then
         FILENAME="\"$(basename "${FILE}")\""
     fi
-    format_file_as_JSON_string "${FILE}"
+    CONTENT="\"$(format_file_as_JSON_string < "${FILE}")\""
 fi
 						     
 # This is the formatting of the JSON request as per the Github Gist API
@@ -124,18 +97,18 @@ fi
 #    }
 #  }
 #}
-echo "{${DESCRIPTION}\"public\": ${PUBLIC}, \"files\": {${FILENAME}: {\"content\": ${CONTENT}}}}" > ${JSON_FILE}
-
-
-# This line has a lot going for it. We send the curl request, with
-# user-authentication if requested, and set up the POST request.
-# We process the response HTML and print the URL of the gist to the user
-curl --silent "${USER}" -X POST -H 'Content-Type: application/json' -d @${JSON_FILE} https://api.github.com/gists | grep "html_url" | head -n 1 | awk '{gsub(/^ +/, "")} 1'
+# This pipeline has a lot going for it. We format the gist as described above
+# send the curl request, with user-authentication if requested, and set up the
+# POST request. We process the response HTML and print the gist's URL to the user
+echo "{${DESCRIPTION}\"public\": ${PUBLIC}, \"files\": {${FILENAME}: {\"content\": ${CONTENT}}}}" \
+    | curl --silent "${USER}" -X POST -H 'Content-Type: application/json' -d @- https://api.github.com/gists \
+    | grep "html_url" \
+    | head -n 1 \
+    | sed '{;s/"//g;s/,$//;s/\s\shtml_url/URL/;}'
 
 # If we could not find the html_url in the response, then we have to tell the
 # user that the http request failed and that his/her gist was not posted
-if [ ${PIPESTATUS[1]} -ne 0 ]; then
-       echo "ERROR: gist failed to post, script exiting..."
-       exit 1
-   fi
-
+if [ ${PIPESTATUS[2]} -ne 0 ]; then
+    echo "ERROR: gist failed to post, script exiting..."
+    exit 1
+fi
