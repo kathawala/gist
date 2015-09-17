361f7f59782f65f43516370b627df213d5ea17b5
Farhan Kathawala <farhank@stanford.edu>
2015-09-17 02:15:06 -0700

    Update pkglist.sh

--- example_uses/pkglist.sh
+++ example_uses/pkglist.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-# Be sure to set USER before using or your gist will be
-# private and anonymous and useless.
 
 set -o errexit
-trap 'rm /tmp/pkglist &> /dev/null' EXIT
 
-# Example: if user is named sonic then line should read USER="-usonic" 
-# (-u must prepend username)
 USER=""
 
-cd /tmp
-pacman -Qqe | grep -v "$(pacman -Qqm)" > pkglist
-echo "===========================================" >> pkglist
-pacman -Qqm >> pkglist
+FILENAME=`mktemp -t pkglist.XXXXXX`
+trap "rm $FILENAME* 2>/dev/null" EXIT
 
-gist.sh ${USER} -p -d "Official pkgs (aur pkgs appended at end)" pkglist
+pacman -Qqe | grep -v "$(pacman -Qqm)" > ${FILENAME}
+echo "===========================================" >> ${FILENAME}
+pacman -Qqm >> ${FILENAME}
+
+gist -u${USER} -p -n "pkglist" -d "Official pkgs (aur pkgs appended at end)" ${FILENAME}
